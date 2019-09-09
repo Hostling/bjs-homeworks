e9a45a9f8762125f54e916dc8b6401c48a5e9af2
Maksim Bondarev <grannas@xaker.ru>
2019-09-09 22:25:16 +0400

    Лекция 1.1 Задание 2

--- variables/main.js
+++ variables/main.js
@@ -5,3 +5,14 @@ let physics = 3;
 let average = Math.floor((algebra + geography + physics)/3);
 
 console.log(average);
+
+
+//___________________________________________________________
+
+let name = 'Вася';
+let message = `Привет, мир! Меня зовут ${name}`;
+
+console.log(message);
+
+
+//___________________________________________________________
